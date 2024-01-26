c7104afde69b8997b3f2ebbd2ee80e1d7c552cfb
Ilya Kuznetsov <ilya@manticoresearch.com>
2024-01-26 13:00:55 +0100

    change CCTZ build option names

--- cmake/GetCCTZ.cmake
+++ cmake/GetCCTZ.cmake
@@ -11,14 +11,12 @@ if(POLICY CMP0135)
     cmake_policy(SET CMP0135 NEW)
 endif()
 
-set ( _stored_BUILD_TESTING ${BUILD_TESTING} )
-set ( _stored_BUILD_EXAMPLES ${BUILD_EXAMPLES} )
-set ( _stored_BUILD_TOOLS ${BUILD_TOOLS} )
-
 set ( CCTZ_URL_GITHUB "https://github.com/manticoresoftware/cctz/archive/refs/heads/master.zip" )
-set ( BUILD_TESTING OFF CACHE BOOL "" FORCE )
-set ( BUILD_EXAMPLES OFF CACHE BOOL "" FORCE )
-set ( BUILD_TOOLS OFF CACHE BOOL "" FORCE )
+
+set ( CCTZ_BUILD_TESTING OFF CACHE BOOL "" FORCE )
+set ( CCTZ_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE )
+set ( CCTZ_BUILD_TOOLS OFF CACHE BOOL "" FORCE )
+set ( CCTZ_BUILD_BENCHMARK OFF CACHE BOOL "" FORCE )
 message ( STATUS "Use cctz from github ${CCTZ_URL_GITHUB}" )
 FetchContent_Declare ( cctz
 		SOURCE_DIR "${CCTZ_SRC}"
@@ -33,10 +31,6 @@ if ( NOT cctz_POPULATED )
 	FetchContent_Populate ( cctz )
 	add_subdirectory ( ${cctz_SOURCE_DIR} ${cctz_BINARY_DIR} )
 endif ()
-
-set ( BUILD_TESTING ${_stored_BUILD_TESTING} CACHE BOOL "" FORCE )
-set ( BUILD_EXAMPLES ${_stored_BUILD_EXAMPLES} CACHE BOOL "" FORCE )
-set ( BUILD_TOOLS ${_stored_BUILD_TOOLS} CACHE BOOL "" FORCE )
 
 include_directories ( "${cctz_SOURCE_DIR}/include" )
 target_link_libraries ( lextra INTERFACE cctz )
